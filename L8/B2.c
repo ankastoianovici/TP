2ea07d928f1aa7d5f15e1aa3cda24d3c5835d872
Anka-Snezana Stoianovici <61665120+ankastoianovici@users.noreply.github.com>
2020-04-28 19:08:56 +0300

    Update B2.c

--- L8/B2.c
+++ L8/B2.c
@@ -1,8 +1,8 @@
 /*Sa se implementeze calculul de factorial sub urmatoarea forma:
-- Functie ìtail-recursiveî
-- Functie ì non-tailed-recursiveî
-- Functie ìdirect recursiveî
-- Functie ìindirect recursiveî*/
+- Functie ‚Äútail-recursive‚Äù
+- Functie ‚Äú non-tailed-recursive‚Äù
+- Functie ‚Äúdirect recursive‚Äù
+- Functie ‚Äúindirect recursive‚Äù*/
 #include<stdio.h>
 #include<stdlib.h>
 int Factorial(int n) //tail
@@ -12,16 +12,12 @@ int Factorial(int n) //tail
 	else
 		return n * Factorial(n - 1);
 }
-int Factorial_(int n) {
-	return (n == 1 || n == 0) ? 1 : n * Factorial_(n - 1);
-}
 int main()
 {
 	int N;
 	printf("N=");
 	scanf("%d", &N);
 	printf("%d!=%d\n", N,Factorial(N));
-	printf("%d!=%d\n", N, Factorial_(N));
 	system("pause");
 	return 0;
 }
